f22ad08a0c04b13fc720432cf187bca066cadf2b
Eher <alexandre@eher.com.br>
2012-04-14 21:36:56 +0000

    Adicionados os outros domnios do servidor

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,7 +10,43 @@ apache::vhost{"chegamos.com":
     replace => true,
 }
 
+apache::vhost{"chegamos.com.br":
+    replace => true,
+}
+
+apache::vhost{"m.chegamos.com":
+    replace => true,
+}
+
 apache::vhost{"encaixote.me":
+    replace => true,
+}
+
+apache::vhost{"eher.com.br":
+    replace => true,
+}
+
+apache::vhost{"alexandreeher.com":
+    replace => true,
+}
+
+apache::vhost{"skd.com.br":
+    replace => true,
+}
+
+apache::vhost{"paginasbrancas.com.br":
+    replace => true,
+}
+
+apache::vhost{"queroservoluntario.com":
+    replace => true,
+}
+
+apache::vhost{"neuroeducacaosorocaba.com.br":
+    replace => true,
+}
+
+apache::vhost{"ervaonline.com.br":
     replace => true,
 }
 

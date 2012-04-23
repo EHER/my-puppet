99945f0e405d9444680b47341387c4bbc943f2ec
Eher <alexandre@eher.com.br>
2012-04-22 23:24:08 -0300

    Organização dos domínios em array

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,52 +1,22 @@
 # site.pp
 include "test"
 include "php"
-include "php::apache"
 include "vim"
 include "mysql"
 include "apache"
 
-apache::vhost{"chegamos.com":
-    replace => true,
+apache::vhost{[
+                "chegamos.com",
+                "chegamos.com.br",
+                "m.chegamos.com",
+                "encaixote.me",
+                "eher.com.br",
+                "alexandreeher.com",
+                "skd.com.br",
+                "paginasbrancas.com.br",
+                "queroservoluntario.com",
+                "neuroeducacaosorocaba.com.br",
+                "ervaonline.com.br"
+            ]:
+            replace => true,
 }
-
-apache::vhost{"chegamos.com.br":
-    replace => true,
-}
-
-apache::vhost{"m.chegamos.com":
-    replace => true,
-}
-
-apache::vhost{"encaixote.me":
-    replace => true,
-}
-
-apache::vhost{"eher.com.br":
-    replace => true,
-}
-
-apache::vhost{"alexandreeher.com":
-    replace => true,
-}
-
-apache::vhost{"skd.com.br":
-    replace => true,
-}
-
-apache::vhost{"paginasbrancas.com.br":
-    replace => true,
-}
-
-apache::vhost{"queroservoluntario.com":
-    replace => true,
-}
-
-apache::vhost{"neuroeducacaosorocaba.com.br":
-    replace => true,
-}
-
-apache::vhost{"ervaonline.com.br":
-    replace => true,
-}
-

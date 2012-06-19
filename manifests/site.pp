bb51461e5a755c3b7610fe269270b59c5e74096d
root <root@domU-12-31-39-09-A9-74.compute-1.internal>
2012-06-19 03:05:03 +0000

    Puppet changed to use nginx and php-fpm

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,59 +1,76 @@
 # site.pp
+
+# apt
 include "apt"
-include "vim"
-include "mysql"
-include "php"
-include "php::pecl::xdebug"
-include "php::pecl::gd"
-include "php::pecl::curl"
-include "php::pecl::sqlite"
-include "git"
-include "apache"
-
-package {"libapache2-mod-php5":
-    ensure => "installed"
-}
-
-apache::module {
-    [
-        "php5",
-        "rewrite"
-    ]:
-    ensure => 'present'
-}
-
 apt::ppa {"ppa:ondrej/php5":}
 
-git::repo {"clone dotVim":
-    target => '/root/.vim',
-    source => 'git://github.com/renanivo/dotvim.git'
-}
-exec {"install dotVim":
-    command => "/usr/bin/make install",
-    cwd => "/root/.vim"
+# php
+package {
+    ["php5-curl", "php5-fpm", "php5-common", "php5", "php5-cli", "php5-sqlite", "php5-gd", "php5-xdebug"]:
+        ensure=> installed
 }
 
-git::repo {"clone queroservoluntario.com":
-    target => '/var/www/queroservoluntario.com',
-    source => 'git://github.com/EHER/voluntarios.git'
-}
-exec {"install queroservoluntario.com":
-    command => "/usr/bin/make install",
-    cwd => "/var/www/queroservoluntario.com"
+file {
+    "/etc/php5/cli/php.ini":
+        source => "/etc/puppet/files/php5/cli/php.ini";
+    "/etc/php5/fpm/php.ini":
+        notify => Service["php5-fpm"],
+        source => "/etc/puppet/files/php5/fpm/php.ini";
+    "/etc/php5/fpm/php-fpm.conf":
+        notify => Service["php5-fpm"],
+        source => "/etc/puppet/files/php5/fpm/php-fpm.conf";
+    "/etc/php5/fpm/pool.d/www.conf":
+        notify => Service["php5-fpm"],
+        source => "/etc/puppet/files/php5/fpm/pool.d/www.conf";
 }
 
-apache::vhost{
-    [
-        "sismo.eher.com.br",
-        "chegamos.com",
-        "chegamos.com.br",
-        "m.chegamos.com",
-        "encaixote.me",
-        "eher.com.br",
-        "alexandreeher.com",
-        "skd.com.br",
-        "paginasbrancas.com.br",
-        "queroservoluntario.com"
-    ]:
-    replace => true,
+service {
+    "php5-fmp":
+        name => "php5-fpm",
+        enable => true,
+        ensure => running,
+        hasrestart => true,
+        require => Package["php5-fpm"];
 }
+
+
+
+# nginx
+package {
+    ["nginx"]:
+        ensure=> installed
+}
+
+file {
+    "/var/www":
+        ensure => present;
+    "/etc/nginx/nginx.conf":
+        notify => Service["nginx"],
+        source => "/etc/puppet/files/nginx/nginx.conf";
+    "/etc/nginx/conf.d/queroservoluntario.com.conf":
+        notify => Service["nginx"],
+        source => "/etc/puppet/files/conf.d/queroservoluntario.conf";
+}
+
+service {
+    "nginx":
+        name => "nginx",
+        enable => true,
+        ensure => running,
+        hasrestart => true,
+        require => Package["nginx"];
+}
+
+
+#    [
+#        "sismo.eher.com.br",
+#        "chegamos.com",
+#        "chegamos.com.br",
+#        "m.chegamos.com",
+#        "encaixote.me",
+#        "eher.com.br",
+#        "alexandreeher.com",
+#        "skd.com.br",
+#        "paginasbrancas.com.br",
+#        "queroservoluntario.com"
+#    ]:
